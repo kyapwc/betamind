242ea251e73fcf4b5047ebf0a7f0c8fad54dbeb0
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-05-29 16:33:33 +0800

    feat: use of useUniqViewSeconds hook in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import Image from "next/image";
+import useUniqViewSeconds from "./hooks/useUniqViewSeconds";
 
 export default function Home() {
+  const values = useUniqViewSeconds()
+  console.log('values: ', values);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
